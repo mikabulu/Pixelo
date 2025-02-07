f85e98c7ca9233147da4ccd3cff741b9256c9718
Anaum Shereef <anaum.ts@gmail.com>
2025-02-07 18:34:56 +0000

    css fix

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -7,13 +7,13 @@
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-2">Email address</label>
                     <input type="email"
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#bfdaa4]">
                 </div>
 
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-2">Password</label>
                     <input type="password"
-                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#bfdaa4]">
                 </div>
 
                 <button
