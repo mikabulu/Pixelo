8a66262be8d2283452eca690f8f2f13f790ee04d
Anaum Shereef <anaum.ts@gmail.com>
2025-04-16 23:56:58 +0100

    signup working

--- frontend/src/views/SignupView.vue
+++ frontend/src/views/SignupView.vue
@@ -30,12 +30,11 @@
                     <label class="block text-sm font-medium text-gray-700 mb-2">Account Type</label>
                     <select v-model="form.account_type"
                         class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#bfdaa4]">
-                        <option disabled value="">Please select one</option>
                         <option value="hobbyist">Hobbyist</option>
                         <option value="professional">Professional</option>
-                        <option value="student">Student</option>
+                        <option value="enthusiast">Enthusiast</option>
                         <option value="studio">Studio</option>
-                        <option value="other">Other</option>
+                        <option value="freelance">Freelance</option>
                     </select>
                 </div>
 
@@ -70,7 +69,7 @@ export default {
                 name: '',
                 password1: '',
                 password2: '',
-                account_type: ''
+                account_type: 'enthusiast'
             },
             errors: []
         }
@@ -107,7 +106,6 @@ export default {
                             this.form.email = ''
                             this.form.password1 = ''
                             this.form.password2 = ''
-
                             this.$router.push({ name: 'login' })
                         } else {
                             if (response.data.errors) {
