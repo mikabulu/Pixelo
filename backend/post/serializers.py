8e35d9ed44a907826823fa94faf6a360ec93246f
Anaum Shereef <anaum.ts@gmail.com>
2025-04-21 23:54:59 +0100

    serializer fix

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -43,10 +43,9 @@ class PostDetailSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
     attachments = PostAttachmentSerializer(read_only = True, many=True)
-    project_tags = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ('id', 'body', 'created_by', 'created_at_formatted','likes_count', 'comments', 'comments_count', 'attachments', 'project_tags') 
+        fields = ('id', 'body', 'created_by', 'created_at_formatted','likes_count', 'comments', 'comments_count', 'attachments') 
 
 class TrendSerializer(serializers.ModelSerializer):
     class Meta:
