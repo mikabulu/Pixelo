e50fe062f146b69496e2b23f438dd68cfb6c6725
Anaum Shereef <anaum.ts@gmail.com>
2025-05-02 23:21:29 +0100

    follow test works

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import User
+from rest_framework.test import APIClient
 
 
 class AuthTest(TestCase):
@@ -34,3 +35,25 @@ class AuthTest(TestCase):
         self.assertEqual(response.status_code, 200)
         # check tokens
         self.assertIn('access', response.data)
+
+
+class FollowTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            email="user1@example.com",
+            name="User One",
+            password="password123"
+        )
+        self.user2 = User.objects.create_user(
+            email="user2@example.com",
+            name="User Two",
+            password="password123"
+        )
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user1)
+    
+    def test_follow_user(self):
+        response = self.client.post(f'/api/followers/follow/{self.user2.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(self.user1.is_following(self.user2))
